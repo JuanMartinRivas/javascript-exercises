054f66d13022e9a42de6a8c0661340ca8ce45c1d
Juan Martín Rivas <jmartinrivas14@gmail.com>
2023-08-11 18:42:10 -0300

    Solving calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  let result = num1 + num2;
+
+  return result
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  let result = num1 - num2;
+
+  return result
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  let result;
+
+  for (let i = 0; i < nums.length; i++ ) {
+    result += nums[i];
+  };
+
+  return result
 };
 
-const multiply = function() {
+const multiply = function (num1, num2) {
+  let result = num1 * num2;
 
+  return result
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  let result = Math.pow(num1, num2);
+
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result;
+
+  if (num === 0) {
+    result = 1
+  } else {
+    for (let i = 2; i <= num; i++)
+      result = result * i;
+  }
+
+  return result
 };
 
 // Do not edit below this line
