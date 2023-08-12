4b44ccefda24422674a322537e7794559a201e39
Juan Martín Rivas <jmartinrivas14@gmail.com>
2023-08-12 11:08:30 -0300

    Completing repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let result = string;
 
+    if (num < 0) {
+        result = 'ERROR';
+    } else if (num == 0) {
+        result = '';
+    } else {
+        for (let i = 1; i < num; i++) {
+            result = result + string;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
