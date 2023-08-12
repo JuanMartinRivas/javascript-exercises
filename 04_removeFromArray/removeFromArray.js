903b8bfa3a071a8e92b27bb465da89147f61e1a7
Juan Martín Rivas <jmartinrivas14@gmail.com>
2023-08-12 15:41:08 -0300

    Completing removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr) {
+    let result = arr;
+    for (y = 1; y < arguments.length; y++) {
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] === arguments[y]) {
+                arr.splice([i], 1);
+                result = arr;
+            }
+        }
+    }
 
+    return result;
 };
 
 // Do not edit below this line
