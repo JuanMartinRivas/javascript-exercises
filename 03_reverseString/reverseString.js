ffeada81dda5d81394d1560ebbe6c780c3c1c546
Juan Martín Rivas <jmartinrivas14@gmail.com>
2023-08-12 11:21:53 -0300

    Completing reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let array = string.split("");
+    let revArr = array.reverse();
+    let result = revArr.join("");
 
-};
+    return result;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
