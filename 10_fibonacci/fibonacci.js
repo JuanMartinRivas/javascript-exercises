474d44d8b15cbff72d8314e62a64e7f48a263422
JuanMartinRivas <jmartinrivas14@gmail.com>
2023-08-26 10:19:56 -0300

    Completing fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    num = Number(num);
+    let term1 = 1;
+    let term2 = 1;
+    let result = 0;
 
+    if (num < 0) {
+        result = "OOPS"
+    }
+
+    for (let i = 2; i < num; i++) {
+        result = term1 + term2;
+        term1 = term2;
+        term2 = result
+    }
+
+    return result
 };
 
 // Do not edit below this line
