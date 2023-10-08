36cd3c5372d7e43d242f4a68fa362f9991d23ae3
JuanMartinRivas <jmartinrivas14@gmail.com>
2023-10-08 19:01:52 -0300

    Changing one line

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const ordered = people.sort(function (first, second){
+        const firstAge = first.yearOfDeath - first.yearOfBirth;
+        const secondAge = second.yearOfDeath - second.yearOfBirth;
+
+        if (firstAge > secondAge){
+            return 1
+        } else {
+            return -1
+        }
+    })
 
 };
 
